a71c8f204f0941729852bf7921225a28240ad071
Olivier Laviale <olivier.laviale@gmail.com>
2017-03-05 10:29:25 +0100

    Refactor ApplicationBindings

--- lib/Binding/ApplicationBindings.php
+++ lib/Binding/ApplicationBindings.php
@@ -1,6 +1,15 @@
 <?php
 
-namespace Icybee\Binding\Core;
+/*
+ * This file is part of the Icybee package.
+ *
+ * (c) Olivier Laviale <olivier.laviale@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Icybee\Binding;
 
 /**
  * {@link \ICanBoogie\Core} prototype bindings.
